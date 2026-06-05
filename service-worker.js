d99e9b1942cb523d18355f29d8a7f563912e0ce5
taoufiksmires-commits <taoufiksmires@gmail.com>
2026-06-05 12:50:41 +0200

    Add service worker

---
+++ service-worker.js
@@ -0,0 +1,36 @@
+const CACHE = 'efi-events-v1';
+const ASSETS = [
+  '/efi-events/',
+  '/efi-events/index.html'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(k => k !== CACHE).map(k => caches.delete(k))
+    )).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  // Toujours réseau en priorité pour les données Supabase
+  if(e.request.url.includes('supabase.co')) {
+    e.respondWith(fetch(e.request));
+    return;
+  }
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        const clone = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+        return res;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
